fe9fac47863852dc11afefa4f94cecd33f416173
fenek644 <fenek644@ya.ru>
2019-05-02 21:51:11 +0300

    Add assosiation and validtions to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  validates :content, length: { maximum: 140 }
+  belongs_to :user
+  validates :content, length: { maximum: 140 }, presence: true
+  validates :user_id, presence: true
 end
